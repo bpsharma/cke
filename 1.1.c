a68d232335fe08407d38775f1eeb1bafb89a466f
Dr B P Sharma <bpsharma@gmail.com>
2021-01-20 13:21:46 +0530

    Add files via upload

---
+++ 1.1.c
@@ -0,0 +1,13 @@
+//Write a program to input a floating number and show that number with two decimal points
+#include<stdio.h>
+int main(){
+	float num;
+	printf("Enter a number : ");scanf("%f",&num);
+	printf("Number is %.2f",num);
+	return 0;
+}
+//Output
+//Enter a number : 567.34556
+//Number is 567.35
+
+
